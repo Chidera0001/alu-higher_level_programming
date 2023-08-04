4e0a1ebc42255d7caa27a8d3a838ca925c3a5847
Chidera0001 <c.anele@alustudent.com>
2023-08-04 03:33:11 -0700

    TASKS

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,3 +1,4 @@
--- Lists all priviledges of SQL users
+-- a script that lists all privileges of the MySQL users:
+-- user_0d_1 and user_0d_2
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
 SHOW GRANTS FOR 'user_0d_2'@'localhost';
