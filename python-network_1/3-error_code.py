db1cfe12691eb93d32813fb2201ade0d40bcbc59
Chidera0001 <c.anele@alustudent.com>
2023-08-04 03:28:09 -0700

    TASKS

--- python-network_1/3-error_code.py
+++ python-network_1/3-error_code.py
@@ -1,34 +1,13 @@
 #!/usr/bin/python3
-'''
-A Python script that takes in a URL, sends a request to the given URL,
-and displays the body of the response (decoded in utf-8).
-'''
-import urllib.error
+""" a Python script that displays the error code """
 import urllib.request
 import sys
 
-if __name__ == "__main__":
-    '''
-    Fetches the response from the provided URL and displays its body.
 
-    Usage:
-    python script.py <URL>
-
-    Parameters:
-        url (str): The URL to send the HTTP request.
-
-    Raises:
-        urllib.error.HTTPError: If the server returns an HTTP error response.
-
-    Example Output:
-        The body of the HTTP response in utf-8 format.
-
-    '''
-    url = sys.argv[1]
-    req = urllib.request.Request(url)
+if __name__ == '__main__':
+    req = urllib.request.Request(sys.argv[1])
     try:
         with urllib.request.urlopen(req) as response:
             print(response.read().decode('utf-8'))
     except urllib.error.HTTPError as e:
-        print(f"Error code: {e.code}")
-
+        print("Error code:", e.code)
--- python-network_1/4-hbtn_status.py
+++ python-network_1/4-hbtn_status.py
@@ -1,18 +1,10 @@
 #!/usr/bin/python3
-'''
-A Python script that fetches https://alx-intranet.hbtn.io/status
-'''
+""" a Python script that fetches https://intranet.hbtn.io/status """
 import requests
 
-if __name__ == "__main__":
-    '''
-    Fetches the status information from https://alx-intranet.hbtn.io/status and prints the response.
 
-    Example Output:
-        Body response:
-            - type: <class 'str'>
-            - content: {"status": "OK"}
-    '''
-    response = requests.get('https://alx-intranet.hbtn.io/status')
-    print(f"Body response:\n\t- type: {type(response.text)}\n\t- content: {response.text}")
-
+if __name__ == '__main__':
+    r = requests.get('https://intranet.hbtn.io/status')
+    print("Body response:")
+    print("\t- type:", type(r.text))
+    print("\t- content:", r.text)
